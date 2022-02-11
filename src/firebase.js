76005472baece5c622828c2db6bfe6db6998bf88
Pazhiloi <martinsen06@gmail.com>
2022-02-11 13:33:56 +0200

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCHccSef7m8jG1-q9TmTNEbv_xkm3r-WL8",
+  authDomain: "tesla-clone-c8649.firebaseapp.com",
+  projectId: "tesla-clone-c8649",
+  storageBucket: "tesla-clone-c8649.appspot.com",
+  messagingSenderId: "22548778567",
+  appId: "1:22548778567:web:63b2e2c27fa5f12aca2b62",
+};
